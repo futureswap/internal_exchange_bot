e8959c8b9d5b65e30131caa77923ab434bd834df
JesseAbram <jesseabramowitz@hotmail.com>
2020-03-04 13:42:09 -0500

    added private key check on spin up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const {getImbalance} = require('./blockchainServices/internalExchange')
 const {checkAndApprove} = require('./blockchainServices/tokenServices')
 const {RERUNTIME} = require('./configurations')
 
+if (!process.env.PRIVATE_KEY) {
+    throw new Error('No private key detected')
+  }    
+  
 const main = async () => {
     await checkAndApprove()
     getImbalance()
